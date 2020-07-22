0610f0772a13bebe3c5e6733c83f1b820f3f9aef
kev-inc <kevinchanxy@gmail.com>
2020-07-23 01:29:14 +0800

    remove redirect

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import AdminNavbar from './AdminNavbar'
-import { Route, useLocation, Redirect } from 'react-router-dom'
+import { Route, useLocation } from 'react-router-dom'
 import DashboardPage from './DashboardPage'
 import ProductsPage from './ProductsPage'
 import AddNewProductPage from './AddNewProductPage'
@@ -13,7 +13,6 @@ function Admin(props) {
       <Route path='/admin/dashboard' component={DashboardPage}/>
       <Route path='/admin/products' component={ProductsPage}/>
       <Route path='/admin/addnewproduct' component={AddNewProductPage}/>
-      <Redirect to='/admin/dashboard'/>
     </div>
   )
 }
